8e4aa5fcdafcc150c3abbf8f6ef8b6645e08b28a
FANT0M100 <giorgie103188@gmail.com>
2024-03-16 19:22:23 +0400

    start meals share

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,26 +1,36 @@
 import Image from "next/image";
 import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
-export default function MealDetailsPage() {
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+
   return (
     <>
       <header className={classes.header}>
         <div className={classes.image}>
-          <Image fill priority />
+          <Image src={meal.image} alt={meal.title} fill />
         </div>
         <div className={classes.headerText}>
-          <h1>TITLE</h1>
+          <h1>{meal.title}</h1>
           <p className={classes.creator}>
-            By <a href={`mailto:${"EMAIL"}`}>NAME</a>
+            By <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
           </p>
-          <p className={classes.summary}>SUMMARY</p>
+          <p className={classes.summary}>{meal.summary}</p>
         </div>
       </header>
       <main>
         <p
           className={classes.instructions}
           dangerouslySetInnerHTML={{
-            __html: "...",
+            __html: meal.instructions,
           }}
         ></p>
       </main>
