260be4f2310879d1008ee8afb3ebc6567918f7a0
FANT0M100 <giorgie103188@gmail.com>
2024-03-16 19:07:43 +0400

    meals details

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,3 +1,29 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+
 export default function MealDetailsPage() {
-  return <h1>Meal details</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill priority />
+        </div>
+        <div className={classes.headerText}>
+          <h1>TITLE</h1>
+          <p className={classes.creator}>
+            By <a href={`mailto:${"EMAIL"}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: "...",
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
