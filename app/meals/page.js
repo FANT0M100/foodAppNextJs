63ab3a55ba77334fcf87d3ed44960d2c7422d79f
FANT0M100 <giorgie103188@gmail.com>
2024-03-16 18:28:12 +0400

    fetching meals data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,23 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Chose your favorite recipes and cook it yourself</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share you favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
